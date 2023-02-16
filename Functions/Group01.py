05d8c2b168b70f5a0e52fe7582d2e164081291d2
Carl Krogmann <contact@carl-krogmann.de>
2023-02-16 16:10:17 +0000

    project files setup

---
+++ Functions/Group01.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+class Group01:
+
+    def __init__(self, name: str):
+        self.name = name
+
+    '''
+    Develop a first method that receives a filename argument and returns a pandas DataFrame.
+    '''
+    def get_data(self) -> str:
+        print('hi' + name)
+
+    '''
+    Develop a second method that outputs a list of the available countries in the data set.
+    '''
+    def get_countries(df: pd.DataFrame) -> list:
+        pass;
+
+    '''
+    Develop a third method that plots a way to correlate the "_quantity" columns.
+    '''
+    def quantity_corr(self, df: pd.DataFrame) -> None:
+        pass;
+
+    '''
+    Develop a fourth method that plots an area chart of the distinct "_output_" columns. 
+    This method should have two arguments: a country argument and a normalize argument. 
+    The former, when receiving NONE or 'World' should plot the sum for all distinct countries. 
+    The latter, if True, normalizes the consumption in relative terms: each year, consumption should always be 100%. The X-axis should be the Year. 
+    The method should return a ValueError when the chosen country does not exist.
+    '''
+    def plot_area_chart(self, country: str, normalize: bool) -> None:
+        pass;
+
+    '''
+    Develop a fifth method that may receive a string with a country or a list of country strings. 
+    This method should compare the total of the "_output_" columns for each of the chosen countries and plot it, so a comparison can be made. 
+    The X-axis should be the Year.
+    '''
+    def plot_area_chart(self, country: str = None,  countries: list = None) -> None:
+
+        if country & countries: # pass a string
+            raise ValueError("Please pass a country or countries");
+
+        elif country: # pass a string
+            pass;
+        elif countries: # pass a list
+            pass;
+        else:
+            raise ValueError("Please pass a country or countries");
+
+    '''
+    Develop a sixth method that must be called gapminder. This is a reference to the famous gapminder tools. 
+    This method should receive an argument year which must be an int. If the received argument is not an int, the method should raise a TypeError. 
+    This method should plot a scatter plot where x is fertilizer_quantity, y is output_quantity, 
+    and the area of each dot should be a third relevant variable you find with exploration of the data.
+    '''
+    def gapminder (self, year: int, df: pd.DataFrame) -> None:
+
+        if isinstance(year, int):
+            pass;
+        else: 
+            raise ValueError("Please pass an integer");
+
+
