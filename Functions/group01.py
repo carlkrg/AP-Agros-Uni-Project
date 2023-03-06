b442424fef2f6448b90b0e60373fd73311555df6
Leon Kahrig <leonkahrig@web.de>
2023-03-06 20:53:44 +0000

    Include the feedback and debbug function get_data, area_chart and gapminder

--- Functions/group01.py
+++ Functions/group01.py
@@ -52,6 +52,8 @@ import requests
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
+from typing import Optional
+
 
 
 class Group01:
@@ -127,11 +129,7 @@ class Group01:
             my_object.get_data()
         """
 
-        url = """   https://
-                    raw.githubusercontent.com/
-                    owid/owid-datasets/master/datasets/
-                    Agricultural%20total%20factor%20productivity%20(USDA)/
-                    Agricultural%20total%20factor%20productivity%20(USDA).csv   """
+        url = "https://raw.githubusercontent.com/owid/owid-datasets/master/datasets/Agricultural%20total%20factor%20productivity%20(USDA)/Agricultural%20total%20factor%20productivity%20(USDA).csv"
 
         if os.path.exists("downloads"):  # check if the downloads directory exists
             print("downloads directory already exists")
@@ -332,6 +330,92 @@ class Group01:
         else:
             raise TypeError("Country does not exist")
 
+    def plot_area_chart2(self, country: Optional[str] = None, normalize: bool = False, optional: Optional[str] = None) -> None:
+            """
+            Plots an area chart of the distinct "_output_" columns for the given country or all
+            countries if `country` is set to "World" or None. The columns are normalized by the total
+            output if the `normalize` parameter is set to True.
+
+            If the dataframe is not available, the method will first call the `get_data` method to
+            download and read the dataset into the `df` attribute.
+
+            Parameters:
+            country (str, optional): 
+                The country to plot the data for. If set to "World" or None,
+                the data for all countries will be plotted.
+            
+            normalize : bool
+                If set to True, the data will be normalized by the total output.
+
+            Raises:
+                TypeError: If the `country` parameter is not a string or the `normalize`
+                parameter is not a bool
+
+                Exception: If one or less columns with the "_output_" suffix are available or if the
+                given country does not exist
+
+            Returns:
+                None
+
+            Example usage:
+                my_object = Group01("my_object")
+                my_object.plot_area_chart("World", True)
+            """
+
+            if country is not None and not isinstance(country, str):
+                raise TypeError("country is not a string, Please pass a string")
+
+            if not isinstance(normalize, bool):
+                raise TypeError("normalize is not a bool, Please pass a bool")
+
+            if self.df is None:
+                self.get_data()
+
+            # Get all columns with "_quantity" suffix and check if there are enough columns
+            column_names = self.df.columns.tolist()
+            df_subset = [c for c in column_names if "_output_" in c]
+            df_subset.append("Year")
+            if len(df_subset) < 2:
+                raise Exception("Not enough columns with '_output' suffix")
+
+            # Plotting function
+            def country_plot(df_temp):
+                norm = ""
+                if normalize:
+                    df_temp["Total"] = (df_temp.iloc[:, :-1]).sum(axis=1)
+                    df_temp.iloc[:, :-2] = (
+                        df_temp.iloc[:, :-2].div(df_temp.Total, axis=0) * 100
+                    )
+                    norm = "% (Normalized)"
+                list_outputs = []
+                list_labels = []
+                for each in df_subset[:-1]:
+                    list_outputs.append(df_temp[each])
+                    list_labels.append(each)
+                plt.stackplot(df_temp.Year, list_outputs, labels=list_labels)
+                plt.set_cmap("Pastel1")
+                plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
+                plt.tick_params(labelsize=12)
+                plt.title("Consumption of agricultural products by country over time")
+                plt.xlabel("Year", size=12)
+                plt.ylabel(("Counsumption" + norm), size=12)
+                plt.ylim(bottom=0)
+                plt.show()
+
+            # Plotting for all countries or a specific country
+            if country in (None, "World"):
+                df_temp = self.df[df_subset].groupby("Year").sum().reset_index()
+                df_temp = df_temp.reindex(
+                    columns=list(df_temp.columns[1:]) + [df_temp.columns[0]]
+                )
+                country_plot(df_temp)
+            elif country in self.get_countries():
+                df_temp = self.df[self.df["Entity"] == country]
+                df_temp = df_temp[df_subset]
+                country_plot(df_temp)
+            else:
+                raise TypeError("Country does not exist")
+
     def plot_country_chart(self, args: Union[list[str], str]) -> None:
         """
         Plots the total of the _output_ values of each selected country given by `country`,
@@ -393,7 +477,7 @@ class Group01:
         plt.ylabel("Total _output")
         plt.show()
 
-    def gapminder(self, year: int) -> None:
+    def gapminder(self, year: int, log_scale: bool = False) -> None:
         """
         Visualize Gapminder data for a specific year.
 
@@ -405,7 +489,8 @@ class Group01:
         Parameters:
         year : int
             The year for which to visualize the data.
-
+        log_scale : bool, optional (default=False)
+            If True, the x and y axis will be displayed in logarithmic scale.
         Raises:
         TypeError
             If the received argument is not an int or if it's negative.
@@ -417,8 +502,8 @@ class Group01:
 
         Example usage:
             my_object = Group01("my_object")
-            my_object.gapminder_plot("World", True)
-        """
+            my_object.gapminder_plot(2000, True)
+        """        
         if not isinstance(year, int) or year < 0:
             raise TypeError("Please pass a positive integer for year")
 
@@ -455,5 +540,102 @@ class Group01:
         plt.grid(True)
         plt.xlabel("fertilizer_quantity", fontsize=14)
         plt.ylabel("output_quantity", fontsize=14)
-        plt.title("Gapminder agriculture", fontsize=20)
+        plt.title(f"Gapminder agriculture - Year {year}", fontsize=20)
+
+        if log_scale:
+            plt.xscale('log')
+            plt.yscale('log')
+            plt.xlabel("fertilizer_quantity_log", fontsize=14)
+            plt.ylabel("output_quantity_log", fontsize=14)
+
         plt.show()
+
+
+    def gapminder2(self) -> None:
+            """
+            Visualize Gapminder data for a specific year.
+
+            This function generates a scatter plot that shows the relationship between fertilizer
+            quantity and output quantity for agriculture in a specific year. The size of each point
+            represents the animal output quantity, which is assumed to be an indicator of the size of
+            the agricultural operation.
+
+            Parameters:
+            year : int
+                The year for which to visualize the data.
+                The user is asked for input the year if the function is
+                calling empty parameters inside the brackets.
+            log_scale : bool
+                If True, the x and y axis will be displayed in logarithmic scale.
+                The user is asked for input in form of y or n. The y stands for yes
+                and set the boolen to True what means the function will use the log scale
+                for the gapminder. If the input it n what stands for no the boolean will
+                set to False and the function will not use the log scale.
+                The brackets of the function has to be empty.
+            Raises:
+            TypeError
+                If the received argument is not an int or if it's negative.
+            ValueError
+                If the year is not present in the dataset.
+
+            Returns:
+                None
+
+            Example usage:
+                my_object = Group01("my_object")
+                my_object.gapminder_plot(2000, True)
+            """
+            year = input("Please enter the year you want to visualize: ")
+            log_scale_input = input("Do you want to use log scale? (y/n): ")
+            if log_scale_input.lower() == "y":
+                log_scale = True
+            else:
+                log_scale = False            
+
+            if not str(year).isdigit() or int(year) < 0:
+                raise TypeError("Please pass a positive integer for year")
+            
+            year = int(year)
+
+            if self.df is None:
+                self.get_data()  # check if df is available
+
+            if year not in self.df["Year"].unique():
+                raise ValueError(f"{year} is not present in the dataset")
+
+            # Increase the graph size
+            plt.figure(dpi=150)
+
+            # Filter data by year
+            year_data = self.df[self.df["Year"] == year]
+
+            # Store animal_output_quantity as a numpy array: np_pop
+            # Exploratory analysis showed that animal_output_quantity is the most relevant variable
+            # regarding their correlation with fertilizer_quantity and output_quantity
+            np_pop = np.array(year_data["animal_output_quantity"])
+            np_pop2 = np_pop * 2
+
+            # Create a scatter plot
+            sns.scatterplot(
+                x="fertilizer_quantity",
+                y="output_quantity",
+                data=year_data,
+                legend=True,
+                size=np_pop2,
+                sizes=(20, 400),
+                alpha=0.5,
+            )
+
+            # Use seaborn scatterplot for better customization
+            plt.grid(True)
+            plt.xlabel("fertilizer_quantity", fontsize=14)
+            plt.ylabel("output_quantity", fontsize=14)
+            plt.title(f"Gapminder agriculture - Year {year}", fontsize=20)
+
+            if log_scale:
+                plt.xscale('log')
+                plt.yscale('log')
+                plt.xlabel("fertilizer_quantity_log", fontsize=14)
+                plt.ylabel("output_quantity_log", fontsize=14)
+
+            plt.show()
